8e4e932edd6d0f9574a92e946b9837d11ed1f889
Moon Studico <moonstudico@gmail.com>
2024-09-17 17:43:27 +0300

    частично

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,28 @@
+type CityCoords = {
+  name: string;
+  coords: string[];
+}
+
+
+export type MockServerData = {
+  title: string[];
+  description: string[];
+  createdDate: string[];
+  city: string[];
+  previewImagePath: string[];
+  photos: string[];
+  isPremium: string[];
+  isFavourites: string[];
+  rating: string[];
+  housingType: string[];
+  roomsCount: string[];
+  visitorsCount: string[];
+  price: string[];
+  conveniences: string[];
+  name: string[];
+  email: string[];
+  avatar: string[];
+  password: string[];
+  type: string[];
+  coords: CityCoords[];
+}
